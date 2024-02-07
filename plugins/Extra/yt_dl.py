d5a7ebe433dbdf67c969791e506faedcc73d5ac9
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-02-07 10:13:57 +0530

    Update yt_dl.py

--- plugins/Extra/yt_dl.py
+++ plugins/Extra/yt_dl.py
@@ -48,7 +48,7 @@ async def song(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [VJ NETWORKS™](https://t.me/vj_bots)**"
+        cap = "**BY›› [𝚃𝚊𝚖𝚒𝚕 𝚁𝚘𝚌𝚔𝚎𝚛𝚜](https://t.me/MV_Rockers)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
