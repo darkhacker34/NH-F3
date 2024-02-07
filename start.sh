20f781d46abf5a0d2aee527275efa9a5b8482195
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2024-02-07 09:32:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
-
 then
-echo "Cloning main Repository"
-git clone https://github.com/darkhacker34/NH-F3/blob/Tech_VJ.git /NH-F3 
-
+  echo "Cloning main Repository"
+  git clone https://github.com/darkhacker34/NH-F3/blob/Tech_VJ.git /NH-F3 
 else
-
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-
   git clone $UPSTREAM_REPO /NH-F3
-
 fi
-
 cd /NH-F3 
-
 pip3 install -U -r requirements.txt
-
 echo "Starting Bot...."
-
 python3 bot.py
