b6b3eaebcdc5e08469b67511655414d7a74ea624
darkhacker34 <72502160+darkhacker34@users.noreply.github.com>
2024-02-07 10:02:43 +0530

    Update lyrics.py

--- plugins/Extra/lyrics.py
+++ plugins/Extra/lyrics.py
@@ -25,9 +25,9 @@ async def sng(bot, message):
           await mee.delete()
           try:
             await mee.delete()
-            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/vj_bots")]]))
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("𝙶𝚛𝚘𝚞𝚙", url = f"t.me/Movie_Laab")]]))
           except Exception as e:                            
-             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/vj_bots")]]))
+             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("𝙲𝚑𝚊𝚗𝚗𝚎𝚕", url = f"t.me/MV_Rockers")]]))
 
 
 def search(song):
